0e68a1ad5fff5c15160e7929ad6f5c2f14325852
dmroserom <65498915+dmroserom@users.noreply.github.com>
2020-07-08 13:34:49 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,28 @@
+## Aim of this function is to 
+## 1. read the household_power_consumption.txt file
+## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
+## 3. generate a histogram of global active power(kilowatts)
+
+## read data
+powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+
+## change class of all columns to correct class
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
+powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
+powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power)
+powerdata$Voltage <- as.numeric(powerdata$Voltage)
+powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity)
+powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
+powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
+powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
+
+#naming and subsetting power consumption data
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#calling the basic plot function
+png("plot1.png", width=480, height=480)
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power") # annotating graph
+dev.off()
